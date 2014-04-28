230aba351d570f0363b0f789c0665b1137ae37b0
agramirez <agramirez2001@gmail.com>
2014-04-28 08:29:05 -0500

    new tests for isNullOrUndefined
    Created basic tests for the isNullOrUndefined function.  Speciically,
    I added new tests for basic functionality with known good args.

--- test/agr-util-test.js
+++ test/agr-util-test.js
@@ -86,3 +86,46 @@ exports.isNull = {
     test.done();
   }
 };
+
+/** tests for the isNullOrUndefined function */
+exports.isNullOrUndefined = {
+  setUp: function (done) {
+    done();
+  },
+  'no args': function (test) {
+    test.expect(1);
+  
+    var expected = true, actual = util.isNullOrUndefined();
+
+    test.equal(expected, actual, 'should return true');
+
+    test.done();
+  },
+  'undefined value': function (test) {
+    test.expect(1);
+
+    var data, expected = true, actual = util.isNullOrUndefined(data);
+
+    test.equal(expected, actual, 'should return true');
+
+    test.done();
+  },
+  'null value': function (test) {
+    test.expect(1);
+
+    var data = null, expected = true, actual = util.isNullOrUndefined(data);
+
+    test.equal(expected, actual, 'should return true');
+
+    test.done();
+  },
+  'empty object': function (test) {
+    test.expect(1);
+
+    var data = {}, expected = false, actual = util.isNullOrUndefined(data);
+
+    test.equal(expected, actual, 'should return false');
+
+    test.done();
+  }
+};
